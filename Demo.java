e4f13804661ca617f19c2573771e3bca6aecf5f8
AmrithaRao <78657987+AmrithaRao@users.noreply.github.com>
2021-02-07 21:31:51 +0530

    demo5

--- Demo.java
+++ Demo.java
@@ -2,8 +2,8 @@ class Demo{
 	public static void main(String args[])
 	{
 	
-		System.out.println("demo2 changed");
-		System.out.println("demo4 changed");
+		
+		System.out.println("demo5 changed");
 	}
 }
 
