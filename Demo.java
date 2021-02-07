7b808b94deb9d6e3a2f19f2e71ba07326636f481
AmrithaRao <78657987+AmrithaRao@users.noreply.github.com>
2021-02-07 21:14:36 +0530

    added new line

--- Demo.java
+++ Demo.java
@@ -2,7 +2,8 @@ class Demo{
 	public static void main(String args[])
 	{
 	
-		System.out.println("demo1 changed");
+		System.out.println("demo2 changed");
+		System.out.println("demo4 changed");
 	}
 }
 
